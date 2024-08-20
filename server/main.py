dfac846714867cdbc61decdfe09faadc8e8e5373
Patilrutuja07 <153291351+Patilrutuja07@users.noreply.github.com>
2024-08-20 23:04:59 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,1 +1,36 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from src.routes.chat import chat
+
+load_dotenv()
+
+api = FastAPI()
+api.include_router(chat)
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+api.mount("/static", StaticFiles(directory="../client/static"), name="static")
+
+@api.get("/test")
+async def root():
+    return {"msg": "API is Online"}
+
+
+if __name__ == "__main__":
+    if os.environ.get('APP_ENV') == "development":
+        uvicorn.run("main:api", host="0.0.0.0", port=3500,
+                    workers=4, reload=True)
+    else:
+      pass
 
