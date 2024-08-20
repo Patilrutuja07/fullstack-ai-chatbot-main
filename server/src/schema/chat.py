537c82be81483c3850d1e54736495ab06e74767c
Patilrutuja07 <153291351+Patilrutuja07@users.noreply.github.com>
2024-08-20 23:09:43 +0530

    Update chat.py

--- server/src/schema/chat.py
+++ server/src/schema/chat.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import List, Optional
+import uuid
 
+class Message(BaseModel):
+    id = uuid.uuid4()
+    msg: str
+    source: str
+    timestamp = str(datetime.now())
+
+
+class Chat(BaseModel):
+    token: str
+    messages: List[Message]
+    name: str
+    session_start = str(datetime.now())
